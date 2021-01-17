7526c261f533759b44499987feeca9d51af25ca3
TonyJem <a.jemanov@gmail.com>
2021-01-17 20:12:05 +0200

    Update ViewController.swift

--- blueCirclesGameXIB/blueCirclesGameXIB/ViewController.swift
+++ blueCirclesGameXIB/blueCirclesGameXIB/ViewController.swift
@@ -20,24 +20,31 @@ class ViewController: UIViewController {
     }
     
     @IBAction func panCircle1Action(_ gesture: UIPanGestureRecognizer) {
+        move(circle1, with: gesture)
     }
     
     @IBAction func panCircle2Action(_ gesture: UIPanGestureRecognizer) {
+        move(circle2, with: gesture)
     }
     
     @IBAction func panCircle3Action(_ gesture: UIPanGestureRecognizer) {
+        move(circle3, with: gesture)
     }
     
     @IBAction func panCircle4Action(_ gesture: UIPanGestureRecognizer) {
+        move(circle4, with: gesture)
     }
     
     @IBAction func panCircle5Action(_ gesture: UIPanGestureRecognizer) {
+        move(circle5, with: gesture)
     }
     
     @IBAction func panCircle6Action(_ gesture: UIPanGestureRecognizer) {
+        move(circle6, with: gesture)
     }
     
     @IBAction func panCircle7Action(_ gesture: UIPanGestureRecognizer) {
+        move(circle7, with: gesture)
     }
     
     private func roundCorners(for circles: [CircleView]) {
@@ -46,7 +53,17 @@ class ViewController: UIViewController {
         }
     }
     
-    @IBAction func panCircle6Action(_ sender: UIPanGestureRecognizer) {
+    private func move(_ justMovedView: CircleView, with gesture: UIPanGestureRecognizer) {
+        let gestureTranslation = gesture.translation(in: view)
+        guard let gestureView = gesture.view else { return }
+        
+        gestureView.center = CGPoint(
+            x: gestureView.center.x + gestureTranslation.x,
+            y: gestureView.center.y + gestureTranslation.y
+        )
+        
+        gesture.setTranslation(.zero, in: view)
+        guard gesture.state == .ended else { return }
     }
     
     @IBAction func panCircle7Action(_ sender: UIPanGestureRecognizer) {
