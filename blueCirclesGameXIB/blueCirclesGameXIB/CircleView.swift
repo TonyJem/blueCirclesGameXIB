1e152378ba03064179496224042c8cb22be76010
TonyJem <a.jemanov@gmail.com>
2021-01-17 18:29:47 +0200

    Minor code cleaning in "CircleView" class

--- blueCirclesGameXIB/blueCirclesGameXIB/CircleView.swift
+++ blueCirclesGameXIB/blueCirclesGameXIB/CircleView.swift
@@ -1,34 +1,34 @@
 import UIKit
 
 class CircleView: UIView {
-
+    
     var workingView: UIView!
-        var xibName: String = "CircleView"
+    var xibName: String = "CircleView"
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setCustomView()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setCustomView()
+    }
+    
+    func getFromXib() -> UIView {
+        let bundle = Bundle(for: type(of: self))
+        let xib = UINib(nibName: xibName, bundle: bundle)
+        let view = xib.instantiate(withOwner: self, options: nil).first as! UIView
         
-        override init(frame: CGRect) {
-            super.init(frame: frame)
-            setCustomView()
-        }
+        return view
+    }
+    
+    func setCustomView() {
+        workingView = getFromXib()
+        workingView.frame = bounds
+        workingView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
         
-        required init?(coder: NSCoder) {
-            super.init(coder: coder)
-            setCustomView()
-        }
-        
-        func getFromXib() -> UIView {
-            let bundle = Bundle(for: type(of: self))
-            let xib = UINib(nibName: xibName, bundle: bundle)
-            let view = xib.instantiate(withOwner: self, options: nil).first as! UIView
-            
-            return view
-        }
-        
-        func setCustomView() {
-            workingView = getFromXib()
-            workingView.frame = bounds
-            workingView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
-            
-            addSubview(workingView)
-        }
-
+        addSubview(workingView)
+    }
+    
 }
