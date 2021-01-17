53549dfb62f5556ca0cc35bfd9db42051aba78f8
TonyJem <a.jemanov@gmail.com>
2021-01-17 18:18:56 +0200

    Add functionality initializating XIB in "CircleView" Class

--- blueCirclesGameXIB/blueCirclesGameXIB/CircleView.swift
+++ blueCirclesGameXIB/blueCirclesGameXIB/CircleView.swift
@@ -2,5 +2,33 @@ import UIKit
 
 class CircleView: UIView {
 
+    var workingView: UIView!
+        var xibName: String = "CircleView"
+        
+        override init(frame: CGRect) {
+            super.init(frame: frame)
+            setCustomView()
+        }
+        
+        required init?(coder: NSCoder) {
+            super.init(coder: coder)
+            setCustomView()
+        }
+        
+        func getFromXib() -> UIView {
+            let bundle = Bundle(for: type(of: self))
+            let xib = UINib(nibName: xibName, bundle: bundle)
+            let view = xib.instantiate(withOwner: self, options: nil).first as! UIView
+            
+            return view
+        }
+        
+        func setCustomView() {
+            workingView = getFromXib()
+            workingView.frame = bounds
+            workingView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+            
+            addSubview(workingView)
+        }
 
 }
