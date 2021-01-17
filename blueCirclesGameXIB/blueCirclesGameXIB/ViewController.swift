ce71787c642af7989c3863c5e54c1a61bda90f68
TonyJem <a.jemanov@gmail.com>
2021-01-17 19:01:14 +0200

    Add outletsfor all circles

--- blueCirclesGameXIB/blueCirclesGameXIB/ViewController.swift
+++ blueCirclesGameXIB/blueCirclesGameXIB/ViewController.swift
@@ -2,6 +2,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var circle1: CircleView!
+    @IBOutlet weak var circle2: CircleView!
+    @IBOutlet weak var circle3: CircleView!
+    @IBOutlet weak var circle4: CircleView!
+    @IBOutlet weak var circle5: CircleView!
+    @IBOutlet weak var circle6: CircleView!
+    @IBOutlet weak var circle7: CircleView!
+    @IBOutlet var circles: [CircleView]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
